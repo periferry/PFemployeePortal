bba7d22d62b4fd02ad5ffc847773a453282f6a8c
Hana <support@periferry.com>
2026-07-20 16:17:49 +0530

    fix

--- build_mac.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Shell helper to build macOS app & DMG installer
-set -e
-
-echo "=== PeriFerry Employee Portal macOS Build ==="
-python3 -m pip install --upgrade pip
-python3 -m pip install requests pywebview pyinstaller pillow pyobjc-framework-Cocoa pyobjc-framework-WebKit
-
-python3 build_mac.py
